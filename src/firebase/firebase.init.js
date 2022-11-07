1affe2cae0a3ba66bc5ec7f31b6741c67607743b
sparkmahadi <mahadi.development@gmail.com>
2022-11-07 21:29:20 +0600

    basic project setup and folders

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmaPSWCaxndPa4Kgx1w9caCON0_NJP_84",
+  authDomain: "enhance-d302c.firebaseapp.com",
+  projectId: "enhance-d302c",
+  storageBucket: "enhance-d302c.appspot.com",
+  messagingSenderId: "228239714067",
+  appId: "1:228239714067:web:3fe711dbd00d1fa86d5ce6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
