bbe1c5d7e4e3e75dc0e5253691ad10ea0125c7c4
sparkmahadi <mahadi.development@gmail.com>
2022-11-09 16:50:40 +0600

    added react-photo-view

--- src/components/Home/HomeChilds/Services.js
+++ src/components/Home/HomeChilds/Services.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Services.css'
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const Services = ({ service }) => {
     const { _id, name, description, img, price } = service;
     return (
         <div className='mx-auto drop-shadow-xl'>
             <div className="max-w-sm min-h-full p-6 rounded-md shadow-md bg-card">
-                <img src={img} alt="" className="object-cover object-center w-full rounded-md h-72 bg-gray-500" />
+                <PhotoProvider>
+                    <PhotoView src={img}>
+                        <img src={img} alt="" className="object-cover object-center w-full rounded-md h-72 bg-gray-500" />
+                    </PhotoView>
+                </PhotoProvider>
                 <div className="mt-6 mb-2">
                     <h2 className="lg:text-xl font-bold tracking-wide">{name}</h2>
                 </div>
